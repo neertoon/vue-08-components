d8b675f4c799ffbb45d846c688c7bb09c2e5c86c
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-09-09 09:31:15 +0200

    Komponent - nowy komponent dla dodawania kontaktu

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,44 @@
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" name="name">
+    </div>
+
+    <div>
+      <label>Phone</label>
+      <input type="text" name="phoneNumber">
+    </div>
+
+    <div>
+      <label>Email</label>
+      <input type="email" name="emailAddress">
+    </div>
+
+    <div>
+      <button>Add contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+  emits: ['save'],
+  data() {
+    return {
+      detailsAreVisible: false,
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    }
+  },
+}
+</script>
